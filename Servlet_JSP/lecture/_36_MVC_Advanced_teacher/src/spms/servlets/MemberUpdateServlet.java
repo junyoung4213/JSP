1ecd44617398433a75474cbe0dc30b50766ac70b
junyoung4213 <junyoung4213@gmail.com>
2020-04-16 23:40:11 +0900

    _36_MVC_Advanced_teacher

---
+++ Servlet_JSP/lecture/_36_MVC_Advanced_teacher/src/spms/servlets/MemberUpdateServlet.java
@@ -0,0 +1,73 @@
+package spms.servlets;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import spms.dao.MemberDao;
+import spms.vo.Member;
+
+@SuppressWarnings("serial")
+@WebServlet("/member/update")
+public class MemberUpdateServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		try {
+			ServletContext sc = this.getServletContext();
+			Connection conn = (Connection) sc.getAttribute("conn");
+
+			MemberDao memberDao = new MemberDao();
+			memberDao.setConnection(conn);
+
+			Member member = memberDao.selectOne(Integer.parseInt(req.getParameter("no")));
+
+			req.setAttribute("member", member);
+
+			RequestDispatcher rd = req.getRequestDispatcher("/member/MemberUpdateForm.jsp");
+			rd.forward(req, resp);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			req.setAttribute("error", e);
+			RequestDispatcher rd = req.getRequestDispatcher("/Error.jsp");
+			rd.forward(req, resp);
+		} finally {
+		}
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		try {
+			ServletContext sc = this.getServletContext();
+
+			Connection conn = (Connection) sc.getAttribute("conn");
+
+			MemberDao memberDao = new MemberDao();
+			memberDao.setConnection(conn);
+
+			memberDao.update(new Member()
+					.setNo(Integer.parseInt(req.getParameter("no")))
+					.setName(req.getParameter("name"))
+					.setEmail(req.getParameter("email")));
+
+			resp.sendRedirect("list");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			req.setAttribute("error", e);
+			RequestDispatcher rd = req.getRequestDispatcher("/Error.jsp");
+			rd.forward(req, resp);
+		} finally {
+		}
+	}
+}
