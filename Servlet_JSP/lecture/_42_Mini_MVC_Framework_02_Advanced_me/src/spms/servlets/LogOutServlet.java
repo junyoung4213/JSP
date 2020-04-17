7c1138de33063dd84668ef2d85f9d5ed46edfe1e
junyoung4213 <junyoung4213@gmail.com>
2020-04-18 00:00:24 +0900

    _42_Mini_MVC_Framework_02_Advanced_me

---
+++ Servlet_JSP/lecture/_42_Mini_MVC_Framework_02_Advanced_me/src/spms/servlets/LogOutServlet.java
@@ -0,0 +1,27 @@
+package spms.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@SuppressWarnings("serial")
+@WebServlet("/auth/logout")
+public class LogOutServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// 세션저장 객체를 무효화
+		HttpSession session = request.getSession();
+		session.invalidate();
+
+		// 다시 로그인 화면으로 이동
+		request.setAttribute("viewUrl", "redirect:login.do");
+	}
+}
