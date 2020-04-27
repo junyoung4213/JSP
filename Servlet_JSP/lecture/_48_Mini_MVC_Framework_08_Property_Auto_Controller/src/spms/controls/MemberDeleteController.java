d50e8eb6b897b4afe96d52a6ad0565902b35f498
junyoung4213 <junyoung4213@gmail.com>
2020-04-28 01:40:39 +0900

    Property_Auto_Controller

---
+++ Servlet_JSP/lecture/_48_Mini_MVC_Framework_08_Property_Auto_Controller/src/spms/controls/MemberDeleteController.java
@@ -0,0 +1,29 @@
+package spms.controls;
+
+import java.util.Map;
+
+import spms.bind.DataBinding;
+import spms.dao.MemberDao;
+
+public class MemberDeleteController implements Controller, DataBinding {
+
+	MemberDao memberDao = null;
+
+	public MemberDeleteController setMemberDao(MemberDao memberDao) {
+		this.memberDao = memberDao;
+		return this;
+	}
+
+	@Override
+	public Object[] getDataBinders() {
+		return new Object[] { "no", Integer.class };
+	}
+
+	@Override
+	public String execute(Map<String, Object> model) throws Exception {
+		Integer no = (Integer) model.get("no");
+		memberDao.delete(no);
+		return "redirect:list.do";
+	}
+
+}
---
+++ Servlet_JSP/lecture/_48_Mini_MVC_Framework_08_Property_Auto_Controller/src/spms/controls/MemberUpdateController.java
@@ -0,0 +1,49 @@
+package spms.controls;
+
+import java.util.Map;
+
+import spms.bind.DataBinding;
+import spms.dao.MemberDao;
+import spms.vo.Member;
+
+public class MemberUpdateController implements Controller, DataBinding {
+
+	MemberDao memberDao = null;
+
+	public MemberUpdateController setMemberDao(MemberDao memberDao) {
+		this.memberDao = memberDao;
+		return this;
+	}
+	
+	// vo 객체를 준비해준다.
+	@Override
+	public Object[] getDataBinders() {
+		return new Object[] {
+				"no",Integer.class,
+				"member",spms.vo.Member.class
+		};
+	}
+
+
+
+	@Override
+	public String execute(Map<String, Object> model) throws Exception {
+		
+		Member member = (Member)model.get("member");
+		
+		
+		// get
+		if (member.getEmail() == null) {
+			Integer no = (Integer) model.get("no");
+			Member detailInfo = memberDao.selectOne(no);
+			model.put("member", detailInfo);
+			return "/member/MemberUpdateForm.jsp";
+		}
+		// post
+		else {
+			memberDao.update(member);
+			return "redirect:list.do";
+		}
+	}
+
+}
